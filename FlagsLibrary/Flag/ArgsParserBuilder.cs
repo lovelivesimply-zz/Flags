4214f4ab736ac8d71cf33bb9701b5dccd4c6088d
lovelivesimply <1047723382@qq.com>
2018-03-21 18:55:12 +0800

    [dandan & wangxue] remove validateFlagName method

--- FlagsLibrary/Flag/ArgsParserBuilder.cs
+++ FlagsLibrary/Flag/ArgsParserBuilder.cs
@@ -9,7 +9,6 @@ namespace Flag
     public class ArgsParserBuilder
     {
         private readonly ArgsParser parser = new ArgsParser();
-        string fullNamePattern = @"^[a-zA-Z0-9_][a-zA-Z0-9_-]*$";
 
         /// <summary>
         /// used to add flag option
@@ -20,29 +19,11 @@ namespace Flag
         /// <returns>return the builder itself, so that we can add more flag option</returns>
         public ArgsParserBuilder AddFlagOption(string fullName, char? abbreviationName, string description)
         {
-
-            ValidFlagName(fullName, abbreviationName);
             parser.flagOptions.Add(new FlagOption(fullName, abbreviationName, description));
             return this;
         }
 
-        void ValidFlagName(string fullName, char? abbreviationName)
-        {
-            if (fullName == null && abbreviationName == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            if (fullName != null && !new Regex(fullNamePattern).IsMatch(fullName))
-            {
-                throw new ArgumentException();
-            }
-
-            if (abbreviationName != null && !Char.IsLetter((char) abbreviationName))
-            {
-                throw new ArgumentException();
-            }
-        }
+        
 
         /// <returns>return a parser</returns>
         public ArgsParser Build()
--- FlagsLibrary/Flag/FlagOption.cs
+++ FlagsLibrary/Flag/FlagOption.cs
@@ -1,13 +1,19 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace Flag
 {
     public class FlagOption
     {
+        string fullNamePattern = @"^[a-zA-Z0-9_][a-zA-Z0-9_-]*$";
+
         public FlagOption()
         {
         }
 
         public FlagOption(string fullName, char? abbreviationName, string description)
         {
+            ValidateFlagName(fullName, abbreviationName);
             FullName = fullName;
             AbbreviationName = abbreviationName;
             Description = description;
@@ -16,5 +22,23 @@ namespace Flag
         public string FullName { get; set; }
         public char? AbbreviationName { get; set; }
         private string Description { get; set; }
+
+        void ValidateFlagName(string fullName, char? abbreviationName)
+        {
+            if (fullName == null && abbreviationName == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (fullName != null && !new Regex(fullNamePattern).IsMatch(fullName))
+            {
+                throw new ArgumentException();
+            }
+
+            if (abbreviationName != null && !Char.IsLetter((char)abbreviationName))
+            {
+                throw new ArgumentException();
+            }
+        }
     }
 }
